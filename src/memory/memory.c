a1759ad450a751615fdae5d498b73888ea3cfce0
141242068-ouxianfei <141242068@smail.nju.edu.cn>
2018-06-23 21:32:51 +0800

    Fixed the unaligned read write bug

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -3,21 +3,20 @@
 
 #define PMEM_SIZE (16 * 1024 * 1024)
 
-#define pmem_rw(addr, type) *(type *)({\
-    Assert(addr < PMEM_SIZE, "physical address(0x%08x) is out of bound", addr); \
-    guest_to_host(addr); \
-    })
-
 uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
   int no = is_mmio(addr);
-  if (no == -1)
-	return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
-  else
+  if (no == -1) {
+    Assert(addr < PMEM_SIZE, "physical address(0x%08x) is out of bound", addr);
+	uint32_t data = 0;
+	memcpy(&data, guest_to_host(addr), 4);
+	return data & (~0u >> ((4 - len) << 3));
+  } else {
 	return mmio_read(addr, len, no);
+  }
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
